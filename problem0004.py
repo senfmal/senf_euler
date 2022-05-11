dfd07b9ad4b1678d37a8967c08fe73c5e10729e8
Michael Veith <michael.veith@veith-consulting.net>
2022-05-11 21:28:55 +0200

    Prepare problem0004
    prepare problem0004, adding comments to problems 1 and 2

---
+++ problem0004.py
@@ -0,0 +1,20 @@
+"""
+Largest palindrome product
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+import StopWatch as sw
+
+stopper = sw.StopWatch("Euler problem 0004 'Largest palindrome product' - Solution")
+
+def solution01():
+    pass
+
+if __name__ == "__main__":
+    stopper.startNewStopper("Solution 1")
+    solution01()
+    stopper.stopCurrentWatch()
+    print(stopper)
