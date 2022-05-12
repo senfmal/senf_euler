bde342ec9916a75c0e2ee78ebff0c43d5167ff69
Michael Veith <michael.veith@veith-consulting.net>
2022-05-12 13:50:17 +0200

    Solution for problem 0004

--- problem0004.py
+++ problem0004.py
@@ -10,11 +10,62 @@ import StopWatch as sw
 
 stopper = sw.StopWatch("Euler problem 0004 'Largest palindrome product' - Solution")
 
+def isPalindrome(num):
+    strNum = str(num)
+    lenNum = len(strNum)
+    if lenNum % 2 == 0:
+        if strNum[0:lenNum//2] == strNum[-1:lenNum//2-1:-1]:
+            return True
+    if strNum[0:lenNum//2] == strNum[-1:lenNum//2:-1]:
+        return True
+    return False
+
+def isPalindrome2(num):
+    strNum = str(num)
+    if strNum == strNum[::-1]:
+        return True
+    return False
+
 def solution01():
-    pass
+    factor1 = 999
+    factor2 = 999
+    product = 0
+    while True:
+        if isPalindrome(factor1 * factor2):
+            if factor1 * factor2 > product:
+                product = factor1 * factor2
+                break
+        factor1 -= 1
+    lowerBound1 = factor1
+    for fact2 in range(factor2, lowerBound1, -1):
+        for fact1 in range(lowerBound1, factor2):
+            if isPalindrome(fact1*fact2):
+                if fact1 * fact2 > product:
+                    product, factor1, factor2 = fact1 * fact2, fact1, fact2
+    print("Solution is: [product] -> {0}, [factors] -> {1} and {2}".format(product, factor1, factor2))
+
+def solution02():
+    factor1 = 999
+    factor2 = 999
+    product = 0
+    while True:
+        if isPalindrome2(factor1 * factor2):
+            if factor1 * factor2 > product:
+                product = factor1 * factor2
+                break
+        factor1 -= 1
+    lowerBound1 = factor1
+    for fact2 in range(factor2, lowerBound1, -1):
+        for fact1 in range(lowerBound1, factor2):
+            if isPalindrome2(fact1*fact2):
+                if fact1 * fact2 > product:
+                    product, factor1, factor2 = fact1 * fact2, fact1, fact2
+    print("Solution is: [product] -> {0}, [factors] -> {1} and {2}".format(product, factor1, factor2))
 
 if __name__ == "__main__":
     stopper.startNewStopper("Solution 1")
     solution01()
+    stopper.startNewStopper("Solution 2 - alternate palindrome check")
+    solution02()
     stopper.stopCurrentWatch()
     print(stopper)
