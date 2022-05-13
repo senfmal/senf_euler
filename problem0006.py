ed068298769ab5b74e1dd3d098ed6a5863d410d5
Michael Veith <michael.veith@veith-consulting.net>
2022-05-13 18:52:01 +0200

    Preparation for problem 0006

---
+++ problem0006.py
@@ -0,0 +1,24 @@
+"""
+The sum of the squares of the first ten natural numbers is,
+1**2 + 2**2 + ... + 10**2 = 385
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)**2 = 55**2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+import StopWatch as sw
+import toolkit as tk
+
+stopper = sw.StopWatch("Euler problem 0005 'Sum square difference' - Solution")
+
+def solution01():
+    pass
+
+if __name__ == '__main__':
+    stopper.startNewStopper("Solution 1")
+    solution01()
+    stopper.stopCurrentWatch()
+    print(stopper)
